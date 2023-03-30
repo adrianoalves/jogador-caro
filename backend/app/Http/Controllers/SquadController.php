e0f4656eccb49a1c4ead835c2eb3574752e5db72
Adriano Alves <tohdi1@gmail.com>
2023-03-30 03:20:30 -0300

    - Squad   - Model   - Squad builder Service   - Collection   - Request for builder endpoint   - Resource   - Controller

---
+++ backend/app/Http/Controllers/SquadController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SquadBuilderRequest;
+use App\Http\Resources\SquadCollection;
+use App\Models\Squad;
+use App\Services\SquadBuilder;
+
+class SquadController extends Controller
+{
+    public function list($match_id)
+    {
+        return new SquadCollection( Squad::fromMatchDay($match_id)->get() );
+    }
+
+    public function mount(SquadBuilderRequest $request)
+    {
+        try {
+            dd($request->all());
+            $matchId = $request->post('match_id');
+            $playersPerSquad = $request->post('players_per_squad', 5);
+
+            $squads = (new SquadBuilder())->handle( $matchId, $playersPerSquad );
+
+            foreach ( $squads as $squad ) {
+                Squad::create([
+                    'match_day_id' => $matchId,
+                    'players' => $squad
+                ]);
+            }
+
+            return new SquadCollection( Squad::fromMatchDay($matchId)->get() );
+        }
+        catch ( \Exception $e ) {
+            return \response()->json(['message' => $e->getMessage()], 421 );
+        }
+    }
+}
---
+++ backend/app/Services/SquadBuilder.php
@@ -0,0 +1,74 @@
+<?php
+namespace App\Services;
+
+use App\Models\Concerns\Position;
+use App\Models\MatchPlayer;
+
+class SquadBuilder
+{
+    public function getConfirmedPlayersByMatch(int $matchId)
+    {
+        return MatchPlayer::confirmed()->fromMatch($matchId)->get()->keyBy('id')->sortByDesc('user.card.overall');
+    }
+
+    /**
+     * @param int $matchId
+     * @return int returns the number of squads related to a match
+     */
+    public function alreadyHasSquadsForMatchDay(int $matchId): int
+    {
+        return \App\Models\Squad::where('match_day_id', $matchId )->count('id');
+    }
+    /**
+     * @param int $matchId the Match day ID
+     * @param int $howManyPlayersPerSquad the number of players by squad
+     * @throws \Exception
+     * @return array
+     */
+    public function handle( int $matchId, int $howManyPlayersPerSquad = 5 ): array
+    {
+        \throw_if( $this->alreadyHasSquadsForMatchDay($matchId), new \Exception('Os times já foram formados para esta Match'));
+
+        $players = $this->getConfirmedPlayersByMatch($matchId);
+        $userAttrs = [ 'id', 'name', 'email' ];
+        $cardAttrs = [ 'primary_position', 'secondary_position', 'overall', 'speed', 'shoot', 'stamina', 'dribble', 'pass'];
+
+        $totalPlayers = $players->count();
+        $howManySquads = \floor($totalPlayers / $howManyPlayersPerSquad);
+        $goalKeepers = $players->where('user.card.primary_position', Position::GOALKEEPER->value);
+        $squads = [];
+
+        for( $i=1; $i <= $howManySquads; $i++ ) {
+
+            $goalKeeper = $goalKeepers->first();
+            if($goalKeeper) {
+                $squads[ $i ] = [
+                    [
+                        'user' => $goalKeeper->user->only($userAttrs),
+                        'card' => $goalKeeper->user->card->only($cardAttrs)
+                    ]
+                ];
+                $players->forget($goalKeeper->id);
+                $goalKeepers->forget($goalKeeper->id);
+            }
+        }
+
+        while ( $totalPlayers > 0 ) {
+            --$totalPlayers;
+            for( $j=1; $j <= $howManySquads; $j++ ) {
+
+                $player = $players->first();
+                if($player) {
+
+                    $squads[ $j ][] = [
+                        'user' => $player->user->only($userAttrs),
+                        'card' => $player->user->card->only($cardAttrs)
+                    ];
+                    $players->forget($player->id);
+                }
+            }
+        }
+
+        return $squads;
+    }
+}
